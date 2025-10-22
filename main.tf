baab7577016308e67a168dc495687bd4798bff6c
adarshmoger <adarshmoger2000@gmail.com>
2025-10-22 15:11:46 +0530

    updated files

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "22_04-lts"
+    sku       = "24_04-lts-gen2"
     version   = "latest"
   }
 

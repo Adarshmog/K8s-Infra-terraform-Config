49092f9e453653944d9e369cbbfde0798dd02e36
adarshmoger <adarshmoger2000@gmail.com>
2025-10-22 14:20:32 +0530

    Updated provider

--- provider.tf
+++ provider.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.38.1"
+      version = "~> 4.13.0"
     }
   }
+  required_version = ">= 1.8.0"
 }
 
 provider "azurerm" {

f4bad1a3fe8ba6908ab658f8dd0f74730d5c5b4d
adarshmoger <adarshmoger2000@gmail.com>
2025-10-22 16:04:51 +0530

    updated

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
-# ========================
+# ------------------------
 # Network Security Group (allow SSH)
-# ========================
+# ------------------------
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.resource_group_name}-nsg"
   location            = var.location
@@ -37,17 +37,24 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
-# ========================
-# Subnet with NSG attached
-# ========================
+# ------------------------
+# Subnet
+# ------------------------
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
 
+# ------------------------
+# Associate NSG with Subnet
+# ------------------------
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+
 
 # ========================
 # Public IPs
